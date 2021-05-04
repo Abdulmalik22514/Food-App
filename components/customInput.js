4848c7c51998a2ee286802a4d2def0ad95a8a79d
Abdulmalik Ibrahim <51162923+Abdulmalik22514@users.noreply.github.com>
2021-05-04 22:56:53 +0100

    feat: created reusable textInput

---
+++ components/customInput.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, TextInput, View } from "react-native";
+
+export const Input = ({ placeholder }) => {
+  return (
+    <View>
+      <TextInput style={styles.textInput} placeholder={placeholder} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  textInput: {
+    borderColor: "#C3C3C3",
+    borderWidth: 1,
+    borderRadius: 15,
+    width: 270,
+    height: 37,
+    alignSelf: "center",
+    paddingHorizontal: 10,
+    marginTop: 13,
+    textAlign: "center",
+  },
+});
