edeffe3672f076a5160c35b94cbafdcf7aeebcf6
Abdulmalik Ibrahim <51162923+Abdulmalik22514@users.noreply.github.com>
2021-05-04 22:57:58 +0100

    chore: assigned the common colors

---
+++ common/colors/index.js
@@ -0,0 +1,7 @@
+export const Background = "#F8F8F8";
+export const Red = "#BB0C24";
+export const Grey = "#C3C3C3";
+export const Ash = "#717171";
+export const Black = "#313131";
+export const itemBack = "#F3F3F3";
+export const blackText = "#3B3B3B";
